843788181b80e7a122113d5b1051f9a296120fae
shambhu_bcoder <shambhunath@bcoder.in>
2022-09-19 13:25:05 +0530

    react create

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="card">
+        <div className='card-qr'>
+          <img className="card-qr-image" src='../src/assets/image-qr-code.png' alt="qr" />
+        </div>
+        <div className='card-text'>
+          <h2>
+            Improve your front-end skills by building projects
+          </h2>
+          <p>Scan the QR code to visit Frontend Mentor  and take your coding  skills to nex level</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+
